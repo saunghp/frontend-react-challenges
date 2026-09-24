93dae822181d0f22805ae668cd220124d9ddb309
saunghp <saunghninphyu122133@gmail.com>
2026-09-24 11:47:37 +0800

    Fix grid

--- social-media-dashboard/social-media/src/components/Overviews.jsx
+++ social-media-dashboard/social-media/src/components/Overviews.jsx
@@ -6,8 +6,7 @@ function Overviews() {
     console.log(data);
   return (
     <>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-10">
-      {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-10"> */}
+    <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6 mb-10">
         {data.overviewToday.map((item,index)=>{
             
         return <Overview key={index} item={item}/>;
--- social-media-dashboard/social-media/src/components/Socials.jsx
+++ social-media-dashboard/social-media/src/components/Socials.jsx
@@ -9,7 +9,6 @@ function Socials() {
     
     <>
     <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-10">
-      {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-10"> */}
         {data.socialProfiles.map((item,index)=>{
         return <Social key={index} item={item}/>;
     })}
